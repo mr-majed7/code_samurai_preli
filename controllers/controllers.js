04cdc2d8d08175cf4e7863ca5dcec1b1c319046b
alam265 <113450350+alam265@users.noreply.github.com>
2024-02-09 17:09:09 +0600

    cntroller added

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,2 +1,17 @@
 
+const Users = require("../models/Users")
+const Station = require("../models/Stations")
 
+
+
+const addUser = async(req, res)=> {
+    const newUser = new Users(req.body)
+    await newUser.save()
+    res.status(201).json(newUser)
+}
+const addStation = async(req, res)=> {
+    const newStation = new Station(req.body)
+    await newStation.save()
+    res.status(201).json(newStation)
+}
+module.exports = {addUser, addStation}
--- routes/stationRouter.js
+++ routes/stationRouter.js
@@ -1,13 +1,10 @@
 //EXTERNAL IMPORTS
 const express = require('express');
+const {addStation} = require("../controllers/controllers")
 
 
 const router = express.Router();
 
-router.post("/", async( req,res, next)=> {
-    const newStation = new Stations(req.body)
-    await newStation.save() 
-    res.send(201).json(newStation)
-})
+router.post("/", addStation)
 
 module.exports = router;
