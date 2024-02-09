558a8fcf69a6b34968584d3574d70a815f41d456
majed <majedulislam7789@gmail.com>
2024-02-09 16:19:08 +0600

    db_design

---
+++ models/Trains.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const stopSchema = new mongoose.Schema({
+    station_id: {
+        type: Number,
+        required: true
+    },
+    arrival_time: {
+        type: String,
+        required: true
+    },
+    departure_time:{
+        type: String,
+        required: true
+    },
+    fare:{
+        type: Number,
+        required: true
+    }
+})
+
+const trainSchema = new mongoose.Schema({
+    train_id: {
+        type: Number,
+        required: true
+    },
+    train_name: {
+        type: String,
+        required: true
+    },
+    capacity:{
+        type: Number,
+        required: true
+    },
+    stops: [stopSchema]
+})
+
+const Train = mongoose.model('Train', trainSchema);
+
+module.exports = Train;
---
+++ models/Users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    user_id: {
+        type: Number,
+        required: true
+    },
+    user_name: {
+        type: String,
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
